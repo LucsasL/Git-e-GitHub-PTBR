c1a36b2084bc60f3f7d385c8ea05587abbd52332
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-07 18:01:24 -0300

    Tentando aplicar JavaScript no menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,15 @@
 function menu() {
-    const EventMenu = document.querySelector('div#nav-button')
-    EventMenu
+    const navbutton = document.querySelector('div#nav-button')
+    const navItems = document.querySelector('.nav-items')
+    const navContainer = document.querySelector('nav#header-nav')
+    
+    if (navItems.style.display == 'block') {
+        navItems.style.display = 'none'
+        navContainer.style.height = '600px'
+        
+    } else {
+        navbutton.style.display = 'block'
+        navItems.style.display = 'block'
+        navItems.style.width = '100%'
+    }
 }
